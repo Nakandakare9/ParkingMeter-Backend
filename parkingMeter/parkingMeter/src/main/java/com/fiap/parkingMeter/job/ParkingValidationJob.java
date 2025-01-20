cdbe10fde523aa298ed490b0b84f5d4571fe028c
Du Nakandakare <nakandakare9@gmail.com>
2025-01-20 01:09:59 -0300

    Ajuste para variaveis de ambiente

--- parkingMeter/parkingMeter/src/main/java/com/fiap/parkingMeter/job/ParkingValidationJob.java
+++ parkingMeter/parkingMeter/src/main/java/com/fiap/parkingMeter/job/ParkingValidationJob.java
@@ -1,48 +1,50 @@
 package com.fiap.parkingMeter.job;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.fiap.parkingMeter.domain.ParkingTransaction;
 import com.fiap.parkingMeter.repository.ParkingTransactionRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Component
 public class ParkingValidationJob {
-	private final ParkingTransactionRepository parkingTransactionRepository;
-	
-	public ParkingValidationJob(ParkingTransactionRepository parkingTransactionRepository) {
+    private final ParkingTransactionRepository parkingTransactionRepository;
+
+    @Value("${parking.validation.expireDuration:5}") 
+    private int expireDurationMinutes;
+
+    @Value("${parking.validation.fixedDelay:3000}") 
+    private long fixedDelay;
+
+    public ParkingValidationJob(ParkingTransactionRepository parkingTransactionRepository) {
         this.parkingTransactionRepository = parkingTransactionRepository;
     }
-	
-	//@Scheduled(fixedDelay = 60000) //Checando a cada 60 segundos
-	@Scheduled(fixedDelay = 3000) //Para teste, checando a cada 3 segundos
+
+    @Scheduled(fixedDelayString = "${parking.validation.fixedDelay:3000}") 
     public void validateParking() {
         LocalDateTime now = LocalDateTime.now();
         System.out.println("Checking parking");
-        
+
         List<ParkingTransaction> parkingsExpire = parkingTransactionRepository.findParkingControlsAboutToExpire(now);
-        
+
         for (ParkingTransaction parkingTransaction : parkingsExpire) {
-            //LocalDateTime expectedExpire = parkingTransaction.getParkingStartTime().plusHours(1); //Definição de duração para o parquimetro de 1 hora
-        	LocalDateTime expectedExpire = parkingTransaction.getParkingStartTime().plusMinutes(5); //Para teste, com duração para o estacionamento de apenas 5 minutos
+            LocalDateTime expectedExpire = parkingTransaction.getParkingStartTime().plusMinutes(expireDurationMinutes);
             if (now.plusMinutes(5).isAfter(expectedExpire)) {
                 sendAlert(parkingTransaction);
             }
         }
-	}
-    
-	//Simula o envio para o serviço de Minutaria para notificar o piloto
+    }
+
     private void sendAlert(ParkingTransaction parkingTransaction) {
-    	System.out.println("--------------------------WARNING--------------------------");
-    	System.out.println("Warning: 5 minutes left before completing each parked hour.");
-        System.out.println("Vehicle: " + parkingTransaction.getDriverVehicle().getDriverVehicleModel());
+        System.out.println("--------------------------WARNING--------------------------");
+        System.out.println("Warning: 5 minutes left before completing each parked hour.");
+        System.out.println("Vehicle Model: " + parkingTransaction.getDriverVehicle().getDriverVehicleModel());
+        System.out.println("Vehicle Plate: " + parkingTransaction.getDriverVehicle().getId().getDriverVehicleLicensePlate());
         System.out.println("Driver: " + parkingTransaction.getDriverVehicle().getDriver().getNameDriver());
         System.out.println("-----------------------------------------------------------");
     }
-        
-        
-
 }
